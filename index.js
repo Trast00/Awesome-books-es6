d1ef67512ff6343b5c0a29dcd89358c220331049
Allassane <dickoallassane1997@gmail.com>
2022-12-05 11:41:07 +0100

    Add New-Book, Contact, Footer Section

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
-import { DateTime } from "luxon";
+import luxon from "./modules/luxon.js";
+import ListBooks from "./modules/ListBook.js";
 
+//var DateTime = luxon.DateTime;
+const pDateTime = document.getElementById('dateTime')
+pDateTime.textContent =  `Date and Time: ${luxon.DateTime.now().toLocaleString(luxon.DateTime.DATETIME_MED) }`
+
+const listBook = new ListBooks()
+
+const form = document.getElementById('addNewBook')
+const [title, author, btnAddBook] = form.elements
+btnAddBook.addEventListener('click', (event) => {
+  const error = document.getElementById('error');
+  if (title.value.length === 0 || author.value.length === 0) {
+    error.classList.remove('hidden');
+    event.preventDefault();
+  } else {
+    error.classList.add('hidden');
+    listBook.add(title.value, author.value);
+  }
+});
+
+//  Add the book in the local storage
+window.addEventListener('beforeunload', () => {
+  localStorage.setItem('listBook', JSON.stringify(listBook.books));
+});
+
+if (window.localStorage.getItem('listBook') !== 'undefined') {
+  const list = JSON.parse(window.localStorage.getItem('listBook'));
+  list.forEach((books) => {
+    listBook.add(books.title, books.author);
+  });
+}
---
+++ modules/ListBook.js
@@ -0,0 +1,41 @@
+import Book from "./Book.js";
+
+export default class ListBooks {
+  constructor() {
+    this.books = [];
+    this.currendID = 0;
+  }
+
+  add(title, author) {
+    const book = new Book(this.currendID, title, author);
+    this.books.push(book);
+    this.displayBook(title, author);
+  }
+
+  delete(id) {
+    document.getElementById(id).remove();
+    this.books = this.books.filter((book) => book.id !== id);
+  }
+
+  displayBook(title, author) {
+    const itemId = this.currendID;
+  
+    const listBooks = document.querySelector('.list-book');
+    const li = document.createElement('li');
+    li.id = itemId;
+    const pTitleAuthor = document.createElement('p');
+    pTitleAuthor.textContent = `"${title}" by ${author}`;
+  
+    const btnRemove = document.createElement('button');
+    btnRemove.textContent = 'Remove';
+  
+    btnRemove.addEventListener('click', () => {
+      listBook.delete(itemId);
+    });
+  
+    li.append(pTitleAuthor, btnRemove);
+  
+    listBooks.append(li);
+    this.currendID += 1;
+  }
+}
